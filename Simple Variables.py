acad0eb804479755fa6d8399824ce0adb45796e4
DestroMetro <shivebariya1@gmail.com>
2025-07-19 15:56:09 +0100

    Add files via upload

---
+++ Simple Variables.py
@@ -0,0 +1,77 @@
+# These are Strings
+
+first_name = "Destro"
+food = "pizushi"
+email = "fakemail@gmail.com"
+
+print(f"Hello, {first_name}.")
+print(f"You like {food}.")
+print(f"Your email is: {email}.")
+
+
+# These are Integers
+# They should not be within quotes, otherwise they would be Strings
+
+age = 60
+quantity = 3
+num_of_students = 25
+
+print(f"You are {age} years old.")
+print(f"You are buying {quantity} items.")
+print (f"There are {num_of_students} students in your class.")
+
+
+# These are Floats
+# They are similar to Integers, but they are decimal numbers
+
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}.")
+print(f"Your GPA is {gpa}.")
+print(f"You ran {5.5}km.")
+
+
+# These are Booleans
+# A boolean is either true or false
+
+is_student = False
+for_sale = False
+is_online = False
+
+if is_student:
+    print ("You are a student")
+else:
+    print ("You are NOT a student")
+if for_sale:
+    print ("That item is for sale")
+else:
+    print ("That item is NOT for sale")
+if is_online:
+    print ("You are online")
+else:
+    print ("You are offline")
+
+# This is a test for myself
+
+user_name = "Falalala"
+year = 2025
+pi = 3.14
+is_admin = False
+
+print (f"Your username is {user_name}.")
+print (f"This year is it {year}.")
+print (f"pi is the number {pi}")
+if is_admin:
+    print ("You are the admin")
+else:
+    print ("You are NOT the admin! Get out!")
+
+# Another thing that is going up
+
+
+
+
+
+
