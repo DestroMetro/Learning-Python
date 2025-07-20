5ca291003dd59f4c43bd090a0c947965522f6351
DestroMetro <shivebariya1@gmail.com>
2025-07-20 12:10:43 +0100

    Add files via upload

---
+++ Basic Arithmetic and Math.py
@@ -0,0 +1,19 @@
+
+friends = 10
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+# friends = friends * 5
+# friends *= 5
+# friends = friends / 5
+# friends /= 5
+# friends = friends ** 2
+# friends **= 2
+
+remainder = friends % 2
+# This can be used to find if a number is even or odd
+
+
+print(remainder)
