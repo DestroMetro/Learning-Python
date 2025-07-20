8687c31d1421b05cc33da5d765b38e50f49aac35
DestroMetro <shivebariya1@gmail.com>
2025-07-20 12:22:20 +0100

    Add files via upload

---
+++ Math Related Functions.py
@@ -0,0 +1,22 @@
+
+x = 3.14
+y = -6
+z = 5
+
+result1 = round(x)
+# The 'round' function rounds the number to the nearest whole number
+
+result2 = abs(y)
+# The 'abs' function is used to find the absolute value of a number, the absolute value is the
+# distance from zero, and is always positive
+
+result3 = pow(z, 3)
+# This would be 5 to the power of 3
+
+result4 = max(x, y, z)
+# This is used to find the maximum value in a list of numbers,
+
+result5 = min(x, y, z)
+# This is used to find the minimum value in a list of numbers
+
+print(result1, result2, result3, result4, result5)
